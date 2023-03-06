d9cbd637f13ae88690f1941ade73738181e36b58
Diego-1984 <diegomenoruceta@gmail.com>
2023-03-06 12:00:50 +0000

    Iniciado

--- src/js/component/semaforo.jsx
+++ src/js/component/semaforo.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-const Semaforo=()=>{
-    return(
-    <>
-    <h1>🟢⚫⚫</h1>
-  
-    
-    </>
-    )
+const Luz1 =()=>  {
+  const [verde, setVerde] = useState("🟢");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (verde < 9) {
+        setVerde(verde +10);
+      } else {
+        setVerde("⚫");
+      }
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [verde]);
+
+  return (
+    <div>
+      <h1>{verde}</h1>
+    </div>
+  );
 }
-export default Semaforo;
+
+export default Luz1;
--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,10 @@ import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
+import Luz1 from "./component/semaforo.jsx";
 
 //import your own components
 import Semaforo from "./component/semaforo.jsx";
 
 //render your react application
-ReactDOM.render(<Semaforo />, document.querySelector("#app"));
+ReactDOM.render(<Luz1 />, document.querySelector("#app"));
